25adef5fbac2524a4168061920b22951c3f334bf
nicolsek <tusek.nicole@gmail.com>
2018-01-17 21:02:54 -0800

    Partially working, need to add promises.

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 /* Node Dependencies */
+var fs = require('fs');
 var request = require('request');
-var form-data = require('form-data');
+
+var FileAPI = require('file-api'),
+    File = FileAPI.File,
+    FileReader = FileAPI.FileReader;
 
 /* ClientID for program */ 
 const clientID = '44707d4b920d48f';
-
-var options = {
-	url: 'https://api.imgur.com/3/image',
-    form: 'image=./testImage.jpg',
-	headers: {
-		'Authorization' : 'Client-ID ' + clientID
-	}
-}
 
 /*
  * @desc Display an error and then exit.
@@ -36,8 +32,54 @@ if (process.argv.length < 3) {
 	err('Not enough arguments.');
 }
 
-function callback(err, resp, body) {
-    console.log(body);
+/*
+ * @desc Read to see what type of file it is.
+ * @param {str} file the file that we'll be checking.
+ */
+
+function read(file) {
+    /* If it's a URL */
+    if (file.includes("http")) {
+        return file
+    } else { /* If it's a file */
+        reader = new FileReader();
+    
+        reader.onload = function(event) {
+            var base64 = event.target.result;
+            return base64.slice(5, -1);
+        }
+
+        reader.readAsDataURL(new File(file));
+    }
 }
 
-request.post(options, callback);
+
+/*
+ * @desc Callback to the request post.
+ * @param {str] err if there is an error.
+ * @param {str} resp way too much details for us ;0.
+ * @param {str} body the resp body.
+ */
+
+function callback(err, resp, body) {
+    if (!err) {
+        log(body);
+    } else {
+        throw err
+    }
+}
+
+for (images = 0; images < process.argv.length - 2; images++) {
+    
+    var options = {
+        url: 'https://api.imgur.com/3/image',
+        form: 'image=' + read(process.argv[images + 2]),
+        headers: {
+            'Authorization' : 'Client-ID ' + clientID
+        }
+    };
+
+    log(read(process.argv[images + 2]));
+
+    request.post(options, callback);
+}
